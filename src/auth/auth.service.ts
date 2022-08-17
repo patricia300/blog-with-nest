feb64be60a54a3af5496c11ac79d0e5a01417bed
patricia300 <patie.leonie@gmail.com>
2022-08-17 22:27:45 +0300

    add role to the payload of jwt.strategy and authServive

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,7 +21,11 @@ export class AuthService {
     }
 
     async login(user: any) {
-        const payload = { username: user.username, sub: user.id }
+        const payload = {
+            username: user.username,
+            sub: user.id,
+            role: user.role,
+        }
         return {
             access_token: this.jwtService.sign(payload),
         }
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,6 +14,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(payload: any) {
-        return { id: payload.sub, username: payload.username }
+        return {
+            id: payload.sub,
+            username: payload.username,
+            role: payload.role,
+        }
     }
 }
