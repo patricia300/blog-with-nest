bbe98760768f1fb792266ccc12fa589e1e322d88
patricia300 <patie.leonie@gmail.com>
2022-08-17 20:01:57 +0300

    edit swagger doc description

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,26 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { link } from 'fs'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule)
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist:true }));
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
-  const config = new DocumentBuilder()
-    .setTitle('Nest API')
-    .setDescription('This is a blog I made with Nest <3')
-    .setVersion('1.0')
-    .build();
+    const config = new DocumentBuilder()
+        .setTitle('Nest API')
+        .setDescription(
+            'This is a blog I made with Nest <3, you can test the version REST API of this app here in the documentation,\n but if you want to try graphQL, go <a link="http://127.0.0.1:3000/graphql"> here!!</a>',
+        )
+        .setVersion('1.0')
+        .build()
 
-  const document = SwaggerModule.createDocument(app, config);
+    const document = SwaggerModule.createDocument(app, config)
 
-  SwaggerModule.setup('/', app, document);
+    SwaggerModule.setup('/', app, document)
 
-  await app.listen(3000);
+    await app.listen(3000)
 }
-bootstrap();
+bootstrap()
