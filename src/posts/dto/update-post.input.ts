bec098d79ae51cf7e3d39c3ec874a039c641e818
patricia300 <patie.leonie@gmail.com>
2022-08-17 20:02:03 +0300

    pretiffy code

--- src/posts/dto/update-post.input.ts
+++ src/posts/dto/update-post.input.ts
@@ -1,36 +1,36 @@
 /* eslint-disable prettier/prettier */
-import { CreatePostInput } from './create-post.input';
-import { InputType, Field, PartialType } from '@nestjs/graphql';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsDate, IsNotEmpty } from 'class-validator';
+import { CreatePostInput } from './create-post.input'
+import { InputType, Field, PartialType } from '@nestjs/graphql'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsArray, IsNotEmpty } from 'class-validator'
 
 @InputType()
 export class UpdatePostInput extends PartialType(CreatePostInput) {
-  @ApiProperty()
-  @IsNotEmpty()
-  @Field(() => String)
-  title: string;
+    @ApiProperty()
+    @IsNotEmpty()
+    @Field(() => String)
+    title: string
 
-  @ApiProperty()
-  @IsNotEmpty()
-  @Field(() => String)
-  category: string;
+    @ApiProperty()
+    @IsNotEmpty()
+    @Field(() => String)
+    category: string
 
-  @ApiProperty({ required: false })
-  @IsArray()
-  @Field(() => [String])
-  tags: string[];
+    @ApiProperty({ required: false })
+    @IsArray()
+    @Field(() => [String])
+    tags: string[]
 
-  @ApiProperty()
-  @IsNotEmpty()
-  @Field(() => String)
-  content: string;
+    @ApiProperty()
+    @IsNotEmpty()
+    @Field(() => String)
+    content: string
 
-  @ApiProperty({ required: false })
-  @Field(() => String)
-  author: string;
+    @ApiProperty({ required: false })
+    @Field(() => String)
+    author: string
 
-  @ApiProperty({ required: false })
-  @Field(() => Boolean)
-  isPublished: boolean;
+    @ApiProperty({ required: false })
+    @Field(() => Boolean)
+    isPublished: boolean
 }
