2b30a1305d5f64413c07c42242967efede638dd3
patricia300 <patie.leonie@gmail.com>
2022-08-18 16:47:16 +0300

    add Apidoc attribut

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { randomInt } from 'crypto'
 import {
     Entity,
@@ -13,38 +14,50 @@ import {
 @Unique(['author', 'title'])
 export class Post {
     @PrimaryGeneratedColumn('uuid')
+    @ApiProperty()
     id: string
 
     @Column()
+    @ApiProperty()
     title: string
 
     @Column({ type: 'text', nullable: false })
+    @ApiProperty()
     content: string
 
     @Column()
+    @ApiProperty()
     category: string
 
     @Column({ default: `anonymous@${randomInt(99, 9999)}` })
+    @ApiProperty()
     author: string
 
     @Column('simple-array')
+    @ApiProperty()
     tags: string[]
 
     @Column({ default: false })
+    @ApiProperty()
     isPublished: boolean
 
     @Column({ default: 0 })
+    @ApiProperty()
     upvote: number
 
     @Column({ default: 0 })
+    @ApiProperty()
     downvote: number
 
     @CreateDateColumn()
+    @ApiProperty()
     createdDate: Date
 
     @UpdateDateColumn()
+    @ApiProperty()
     updatedDate: Date
 
     @DeleteDateColumn()
+    @ApiProperty()
     deletedDate: Date
 }
