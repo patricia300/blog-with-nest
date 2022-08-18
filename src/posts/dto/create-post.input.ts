537f2d8ef11e404c061739c8475c0a80ee9e87ba
patricia300 <patie.leonie@gmail.com>
2022-08-18 17:24:39 +0300

    Input type for posts

--- src/posts/dto/create-post.input.ts
+++ src/posts/dto/create-post.input.ts
@@ -1,38 +1,30 @@
-import { randomInt } from 'crypto'
-import { InputType, Field } from '@nestjs/graphql'
-import { ApiProperty } from '@nestjs/swagger'
-import { IsArray, IsBoolean, IsNotEmpty, IsString } from 'class-validator'
+import { InputType, Int, Field } from '@nestjs/graphql'
+import { IsNotEmpty, IsString, IsArray, IsBoolean } from 'class-validator'
 
 @InputType()
 export class CreatePostInput {
-    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     @Field(() => String)
     title: string
 
-    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     @Field(() => String)
     category: string
 
-    @ApiProperty({ required: false })
     @IsArray()
     @Field(() => [String], { nullable: 'itemsAndList' })
     tags: string[]
 
-    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     @Field(() => String)
     content: string
 
-    @ApiProperty({ required: false })
     @Field(() => String, { nullable: true })
     author: string
 
-    @ApiProperty({ required: false })
     @IsBoolean()
     @Field(() => Boolean, { defaultValue: false })
     isPublished: boolean
