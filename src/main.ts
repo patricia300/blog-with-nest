e0f1745b88fd4b1433d2eafe202f618c81fc4190
patricia300 <patie.leonie@gmail.com>
2022-08-18 16:57:17 +0300

    delete useless config and file to graphql

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Nest API')
-    .setDescription('This is a blog I made with Nest <3')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('/', app, document);
 
   await app.listen(3000);
 }
