a7d06517e64fdac9693c5307ef1188be3f929a01
patricia300 <patie.leonie@gmail.com>
2022-08-18 16:50:27 +0300

    add Swagger doc and some excepton to controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,3 +1,4 @@
+import { Post as Article } from './entities/post.entity'
 import {
     Controller,
     Get,
@@ -7,47 +8,82 @@ import {
     Param,
     Delete,
     ParseUUIDPipe,
+    BadRequestException,
+    NotFoundException,
+    Put,
 } from '@nestjs/common'
 import { PostsService } from './posts.service'
 import { CreatePostDto } from './dto/create-post.dto'
 import { UpdatePostDto } from './dto/update-post.dto'
-import { ApiTags } from '@nestjs/swagger'
+import {
+    ApiBadRequestResponse,
+    ApiNotFoundResponse,
+    ApiOkResponse,
+    ApiTags,
+} from '@nestjs/swagger'
 @ApiTags('Posts')
 @Controller('posts')
 export class PostsController {
     constructor(private readonly postsService: PostsService) {}
 
     @Post()
-    create(@Body() createPostDto: CreatePostDto) {
-        return this.postsService.create(createPostDto)
+    @ApiBadRequestResponse({ description: 'Bad request' })
+    @ApiOkResponse({ type: Article })
+    async create(@Body() createPostDto: CreatePostDto) {
+        const post = await this.postsService.create(createPostDto)
+        if (!post) throw new BadRequestException()
+        return post
     }
 
     @Get()
+    @ApiOkResponse({ type: [Article] })
     findAll() {
         return this.postsService.findAll()
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.postsService.findOne(id)
+    @ApiBadRequestResponse({ description: 'Bad request' })
+    @ApiNotFoundResponse({ description: 'Not found post' })
+    @ApiOkResponse({ type: Article })
+    async findOne(@Param('id', ParseUUIDPipe) id: string) {
+        const post = await this.postsService.findOne(id)
+        if (!post) throw new NotFoundException()
+        return post
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-        return this.postsService.update(id, updatePostDto)
+    @ApiNotFoundResponse({ description: 'Not found post' })
+    @ApiBadRequestResponse({ description: 'Bad request' })
+    @ApiOkResponse({ type: Article })
+    async update(
+        @Param('id') id: string,
+        @Body() updatePostDto: UpdatePostDto,
+    ) {
+        const post = await this.postsService.update(id, updatePostDto)
+        if (!post) throw new NotFoundException()
+        return post
     }
 
     @Delete(':id')
-    remove(@Param('id') id: string) {
+    @ApiNotFoundResponse({ description: 'Not found post' })
+    @ApiBadRequestResponse({ description: 'Bad request' })
+    @ApiOkResponse({ type: Article })
+    remove(@Param('id', ParseUUIDPipe) id: string) {
         return this.postsService.remove(id)
     }
 
     @Get('upvote/:id')
+    @ApiBadRequestResponse({ description: 'Bad request' })
+    @ApiNotFoundResponse({ description: 'Not found post' })
+    @ApiOkResponse({ type: Article })
     upvote(@Param('id', new ParseUUIDPipe()) id: string) {
         return this.postsService.upvote(id)
     }
 
     @Get('downvote/:id')
+    @ApiBadRequestResponse({ description: 'Bad request' })
+    @ApiNotFoundResponse({ description: 'Not found post' })
+    @ApiOkResponse({ type: Article })
     downvote(@Param('id', new ParseUUIDPipe()) id: string) {
         return this.postsService.downvote(id)
     }
