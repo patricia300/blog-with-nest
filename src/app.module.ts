ff30b6a00187769211db47010f3b23a550aa99a8
patricia300 <patie.leonie@gmail.com>
2022-08-18 15:04:42 +0300

    delete graphql Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,9 @@
-import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+/* eslint-disable prettier/prettier */
+
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import config from 'orm.config';
-import { join } from 'path';
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(config),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(config)],
 })
 export class AppModule {}
