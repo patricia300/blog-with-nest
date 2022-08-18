5917600d427b62a131b140f7adf7ea516a2d5179
patricia300 <patie.leonie@gmail.com>
2022-08-18 18:26:15 +0300

    create and add login.dto to controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,21 @@ import { JwtAuthGuard } from './jwt-auth.guard'
 import { AuthService } from './auth.service'
 import { LocalAuthGuard } from './local-auth.guard'
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
-import { ApiBody, ApiTags } from '@nestjs/swagger'
+import {
+    ApiBearerAuth,
+    ApiBody,
+    ApiHeader,
+    ApiSecurity,
+    ApiTags,
+} from '@nestjs/swagger'
+import { LoginDto } from './dto/login.dto'
 
 @Controller()
 @ApiTags('Auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
     @UseGuards(LocalAuthGuard)
+    @ApiBody({ type: LoginDto })
     @Post('/auth/login')
     async login(@Request() req): Promise<any> {
         return this.authService.login(req.user)
